2f7336f02ff0694bcec91845a5bfad85d6819393
Odulaja oluwadamilare O <damijustice1@gmail.com>
2022-10-12 19:50:32 +0100

    completed user authentication

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,12 +17,21 @@ export function signInAPI() {
   };
 }
 
+export function signOutAPI() {
+  return (dispatch) => {
+    auth
+      .signOut()
+      .then(() => { dispatch(setUser(null))})
+      .catch((error) => alert(error.message));
+  };
+}
+
 export function getUserAuth() {
   return (dispatch) => {
     auth.onAuthStateChanged(async (user) => {
-        if (user) {
-            dispatch(setUser(user))
-        }
+      if (user) {
+        dispatch(setUser(user));
+      }
     });
   };
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import thunkMiddleWare from "redux-thunk";
 import rootReducer from "../reducers"
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare))
+const store =  createStore(rootReducer, applyMiddleware(thunkMiddleWare))
 
 export default store
